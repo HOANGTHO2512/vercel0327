d4b19fabdaddb32663ecec1c291f6d2651bfeed2
HOANGTHO2512 <96385232+HOANGTHO2512@users.noreply.github.com>
2025-03-27 11:48:41 +0800

    2

--- index.py
+++ index.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>黃紫凌Python網頁2</h1>"
+    homepage = "<h1>黎黃壽Python網頁+8</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=紫凌&work=pu>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=黃壽&work=pu>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
-    homepage += "<a href=/about>紫凌簡介網頁</a><br>"
+    homepage += "<a href=/about>黃壽簡介網頁</a><br>"
     return homepage
 
 
